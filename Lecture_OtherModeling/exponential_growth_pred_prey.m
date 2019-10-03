9998f70aa13a39b8efeaf147dae9ae5efc8f381b
Jon Robinson <jonrob@chalmers.se>
2019-10-03 16:30:15 +0200

    refactor: exponential_growth_pred_prey several function updates

--- Lecture_OtherModeling/exponential_growth_pred_prey.m
+++ Lecture_OtherModeling/exponential_growth_pred_prey.m
@@ -1,53 +1,87 @@
 function [t,x] = exponential_growth_pred_prey(cell_init, growth_rate ,death_rate)
-% Author: Daniel Cook
-% Date: 10/01/2018
+%Simulate exponential growth with predator-prey dynamics
+%
+% Input:
+%
+%   cell_init       vector specifying the initial number of the predator
+%                   and prey cells, respectively (e.g., [10, 10])
+%
+%   growth_rate     vector specifying the growth rate (1/h) for the
+%                   predator and prey cells, respectively (e.g., [0.005, 0.2])
+%
+%   death_rate      vector specifying the death rate (1/h) for the
+%                   predator and prey cells, respectively (e.g., [0.1, 0.01])
+%
+% Output:
+%
+%   t       vector of time points (h)
+%
+%   x       Nx2 matrix of the number of predator and prey cells at each
+%           time point
+%
+% Usage:
+%
+%   [t,x] = exponential_growth_pred_prey(cell_init, growth_rate, death_rate);
+%
+%
+%
+% Author: Daniel Cook, 2018-10-01
+% Updated: Jonathan Robinson, 2019-10-03
 % Copyrighted under Creative Commons Share Alike
-% To run, use: [t,x] = exponential_growth_pred_prey([10,10],[.2,.005],[.01,.1]);
 
-% Set plotting and printing (1=show results, 2=suppress results)
-shouldPlot = 1; colorChoice = 'k';
 
 %% Section 1: Set parameter values
-% Set time (in days)
-tStart = 0;
-tEnd = 100; % Number of hours to run simulation
 
-% Set number of cells to start
-x0(1) = cell_init(1); % cell population 1
-x0(2) = cell_init(2); % cell population 2
+%Set plotting and printing (true=show results, false=suppress results)
+shouldPlot = true;
+
+% Set start and end times (in hours), as well as the step size
+tStart = 0;
+tEnd = 100;
+tStep = 0.1;
+
+% Set number of cells to start with
+x0(1) = cell_init(1);  % predator
+x0(2) = cell_init(2);  % prey
 
 % Define model parameters
-k = [growth_rate,death_rate];
+k = growth_rate;
+d = death_rate;
+
 
 %% Section 2: Run model
+
 % Call ODE solver
-timeStep = 0.1; % Days
-[t,x] = ode45(@(t,x)odefun(t,x,k,x0), [tStart:tEnd], x0);
+[t,x] = ode45(@(t,x) growthFunc(t,x,k,d,x0), tStart:tStep:tEnd, x0);
+
 
 %% Section 3: Plot results
 % Plot results
 if shouldPlot == 1
-    % Plot cell growth
-    figure(1); hold on; plot(t,x(:,1),'-','color',colorChoice,'linewidth',2);
-    figure(1);hold on; plot(t,x(:,2),'--','color',colorChoice,'linewidth',2);
-    set(gca,'fontsize',18,'linewidth',2); box off
-    xlabel('Time (hours)'); ylabel('Level [A.U.]')
-    legend('x1:Prey','x2:Predator')
+    plot(t,x,'LineWidth',2);
+    set(gca,'fontsize',14);
+    xlabel('Time (hours)');
+    ylabel('Level [A.U.]');
+    legend('cell 1','cell 2');
 end
 
 end
 
-function dxdt = odefun(t,x,k,x0)
+
+%% Secion 4: Growth function
+function dxdt = growthFunc(t,x,k,d,x0)
 % This function is the growth equation
 
 % Set parameters
-growth_rate = k(1:2);
-death_rate = k(3:4);
+growth_rate = k;
+death_rate = d;
 
 % Set up differential equations
-dxdt(1) = growth_rate(1)*x(1) - death_rate(1)*x(1)*x(2); % Cell population (1)
-dxdt(2) = growth_rate(2)*x(2)*x(1) - death_rate(2)*x(2); % Cell population (2)
-
+dxdt(1) = growth_rate(1)*x(1)*x(2) - death_rate(1)*x(1); % Predator population
+dxdt(2) = growth_rate(2)*x(2) - death_rate(2)*x(2)*x(1); % Prey population
 
 dxdt = dxdt'; % This needs to be a column vector
 end
+
+
+
