9bcdbd01acca04c5d3fc01a745bc40b995e6c59e
Jon Robinson <jonrob@chalmers.se>
2019-10-03 16:54:00 +0200

    feat: stochastic_pred_prey updated code and fixed error in algorithm
    Reaction probabilities were incorrectly calculated

--- Lecture_OtherModeling/stochastic_pred_prey.m
+++ Lecture_OtherModeling/stochastic_pred_prey.m
@@ -1,73 +1,99 @@
 function [t,x] = stochastic_pred_prey(cell_init, growth_rate ,death_rate)
-% Author: Daniel Cook
-% Date: 10/01/2018
+%Simulate stochastic predator-prey dynamics
+%
+% Input:
+%
+%   cell_init       vector specifying the initial number of the predator
+%                   and prey cells, respectively (e.g., [10, 10])
+%
+%   growth_rate     vector specifying the growth rate (1/h) for the
+%                   predator and prey cells, respectively (e.g., [0.005, 0.2])
+%
+%   death_rate      vector specifying the death rate (1/h) for the
+%                   predator and prey cells, respectively (e.g., [0.1, 0.01])
+%
+% Output:
+%
+%   t       vector of time points (h)
+%
+%   x       Nx2 matrix of the number of predator and prey cells at each
+%           time point
+%
+% Usage:
+%
+%   [t,x] = stochastic_pred_prey(cell_init, growth_rate, death_rate);
+%
+%
+%
+% Author: Daniel Cook, 2018-10-01
+% Updated: Jonathan Robinson, 2019-10-03
 % Copyrighted under Creative Commons Share Alike
-% To run, use: [t,x] = stochastic_pred_prey([10,10],[.2,.06],[.01,.1]);
 
-% Set plotting and printing (1=show results, 2=suppress results)
-shouldPlot = 0; colorChoice = 'k';
 
 %% Section 1: Set parameter values
-% Set time (in days)
+
+%Set plotting and printing (true=show results, false=suppress results)
+shouldPlot = true;
+
+% Set start and end times (in hours), as well as the step size
 tStart = 0;
-tEnd = 500; % Number of hours to run simulation
+tEnd = 500;
 
-% Set number of cells to start
-x(1,1) = cell_init(1); % cell population 1
-x(2,1) = cell_init(2); % cell population 2
+% Set number of cells to start with
+x = cell_init;
 
-% Define model parameters
-k = [growth_rate,death_rate];
 
 %% Section 2: Run model
-t_step = tStart;
-t(1) = t_step;
+t_current = tStart;
+t(1) = t_current;
 i = 1;
-while t_step <= tEnd
-    % Which reaction will occur
+while t_current <= tEnd
+    % Determine which reaction(s) will occur
     % Set probabilities
-    p1 = growth_rate(1)*x(1,i); % Prey birth
-    p2 = death_rate(1)*x(1,i)*x(2,i); % Prey death
-    p3 = growth_rate(2)*x(1,i)*x(2,i); % Predator birth
-    p4 = death_rate(2)*x(2,i); % Predator death
+    p1 = growth_rate(1) * x(i,2) * x(i,1); % Predator birth
+    p2 = death_rate(1) * x(i,1); % Predator death
+    p3 = growth_rate(2) * x(i,2); % Prey birth
+    p4 = death_rate(2) * x(i,2) * x(i,1); % Prey death
     pTot = p1 + p2 + p3 + p4; % Total of rates
     
     % Generate random number (uniform random variable)
     reaction_rn = rand;
     
     % Select reaction
-    if reaction_rn < p1/pTot
-        x(1,i+1) = x(1,i) + 1;
-        x(2,i+1) = x(2,i);
-    elseif reaction_rn >= p1/pTot && reaction_rn < p2/pTot
-        x(1,i+1) = x(1,i) - 1;
-        x(2,i+1) = x(2,i);
-    elseif reaction_rn >= p2/pTot && reaction_rn < p3/pTot
-        x(1,i+1) = x(1,i);
-        x(2,i+1) = x(2,i) + 1;
-    else
-        x(1,i+1) = x(1,i);
-        x(2,i+1) = x(2,i) - 1;
+    if reaction_rn < p1/pTot  % Predator birth
+        x(i+1,1) = x(i,1) + 1;
+        x(i+1,2) = x(i,2);
+    elseif reaction_rn < (p1+p2)/pTot  % Predator death
+        x(i+1,1) = x(i,1) - 1;
+        x(i+1,2) = x(i,2);
+    elseif reaction_rn < (p1+p2+p3)/pTot  % Prey birth
+        x(i+1,1) = x(i,1);
+        x(i+1,2) = x(i,2) + 1;
+    else                                % Prey death
+        x(i+1,1) = x(i,1);
+        x(i+1,2) = x(i,2) - 1;
     end
     
     % How long will a reaction take (exponential random variable)
-    tau = exprnd(pTot);
-%     tau = exprnd(1/pTot);
+    tau = exprnd(1/pTot);
 
-    t_step = t_step+tau;
-    t(i+1) = t_step;
+    % step forward in time
+    t_current = t_current+tau;
+    t(i+1,1) = t_current;
     i = i+1;
 end
 
 %% Section 3: Plot results
 % Plot results
-if shouldPlot == 1
-    % Plot cell growth
-    figure(1); hold on; plot(t,x(1,:),'-','color',colorChoice,'linewidth',2);
-    figure(1);hold on; plot(t,x(2,:),'--','color',colorChoice,'linewidth',2);
-    set(gca,'fontsize',18,'linewidth',2); box off
-    xlabel('Time (hours)'); ylabel('Level [A.U.]')
-    legend('x1:Prey','x2:Predator')
+if shouldPlot
+    plot(t,x,'LineWidth',2);
+    set(gca,'fontsize',14);
+    xlabel('Time (hours)');
+    ylabel('Level [A.U.]');
+    legend('Predator','Prey');
 end
 
+
 end
+
+
