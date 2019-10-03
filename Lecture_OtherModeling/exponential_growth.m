c25e40dbbc244453a01511fa45cda37c8a0115e2
Jon Robinson <jonrob@chalmers.se>
2019-10-03 15:38:31 +0200

    refactor: exponential_growth several modifications to code and comments

--- Lecture_OtherModeling/exponential_growth.m
+++ Lecture_OtherModeling/exponential_growth.m
@@ -1,45 +1,76 @@
 function [t,x] = exponential_growth(cell_init, growth_rate)
-% Author: Daniel Cook
-% Date: 10/01/2018
-% Copyrighted under Creative Commons Share Alike
-% To run, use: [t,x] = exponential_growth(1,.2);
+%Simulate exponential growth
+%
+% Input:
+%
+%   cell_init       initial number of cells (e.g., 1)
+%
+%   growth_rate     growth rate (1/h) (e.g., 0.2)
+%
+% Output:
+%
+%   t       vector of time points (h)
+%
+%   x       vector of number of cells at each time point
+%
+% Usage:
+%
+%   [t,x] = exponential_growth(cell_init, growth_rate)
+%
+%
+%
+% Author: Daniel Cook, 2018-10-01
+% Updated: Jonathan Robinson, 2019-10-03
+% Copyrighted under Creative Commons Share Alike 
 
-% Set plotting and printing (1=show results, 2=suppress results)
-shouldPlot = 1; colorChoice = 'k';
 
 %% Section 1: Set parameter values
-% Set time (in hours)
-tStart = 0;
-tEnd = 48; % Number of hours to run simulation
 
-% Set number of cells to start
+%Set plotting and printing (true=show results, false=suppress results)
+shouldPlot = true;
+
+% Set start and end times (in hours), as well as the step size
+tStart = 0;
+tEnd = 48;
+tStep = 0.1;
+
+% Set number of cells to start with
 x0 = cell_init;
 
 % Define model parameters
 k = growth_rate;
 
+
 %% Section 2: Run model
+
 % Call ODE solver
-[t,x] = ode45(@(t,x)odefun(t,x,k,x0), [tStart:tEnd], x0);
+[t,x] = ode45(@(t,x) growthFunc(t,x,k,x0), tStart:tStep:tEnd, x0);
+
 
 %% Section 3: Plot results
 % Plot results
-if shouldPlot == 1
-    % Plot cell growth
-    figure(1); hold on; plot(t,x,'-','color',colorChoice,'linewidth',2);
-    set(gca,'fontsize',18,'linewidth',2); box off
-    xlabel('Time (hours)'); ylabel('Number of cells')
-%     legend('x1')
+if shouldPlot
+    plot(t,x,'LineWidth',2);
+    set(gca,'fontsize',14);
+    xlabel('Time (hours)');
+    ylabel('Number of cells');
 end
 
 end
 
-function dxdt = odefun(t,x,k,x0)
+
+%% Secion 4: Growth function
+
+function dxdt = growthFunc(t,x,k,x0)
 % This function is the growth equation
 
 % Set parameters
 growth_rate = k;
 
 % Set up differential equations
-dxdt = growth_rate*x;
+dxdt = growth_rate * x;
 end
+
+
+
+
