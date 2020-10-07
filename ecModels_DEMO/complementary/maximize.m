52ac8e3ace31bd97ad4cd4a39ed64ce61f2e8461
Ivan Domenzain <ivand@chalmers.se>
2020-10-07 17:22:25 +0200

    chore: remove unnecessary files

--- ecModels_DEMO/complementary/maximize.m
+++
@@ -1,5 +0,0 @@
-% Maximize:
-function v = maximize(c,S,b,LB,UB)
-[v,~,flag] = linprog(-c,[],[],S,b,LB,UB);   %-c to maximize instead of minimize
-v          = v*(flag == 1);                 %Avoids plotting infeasible problems
-end
